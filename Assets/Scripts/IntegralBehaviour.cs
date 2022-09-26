5192d7ee778840ffabd195c32a3ae360428e8f2a
FeSanz <68288843+FeSanz@users.noreply.github.com>
2022-09-26 14:34:15 -0500

    Desarrollos integrales 1
    Listo

---
+++ Assets/Scripts/IntegralBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntegralBehaviour : MonoBehaviour
+{
+    [SerializeField, Tooltip("Objetos del menu")] private GameObject[] menu;
+
+    [SerializeField, Tooltip("Objetos del contenido")] private GameObject[] content;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
