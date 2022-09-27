9a41de43cdf54c844ec1ca34ff2bc681741a5278
FeSanz <68288843+FeSanz@users.noreply.github.com>
2022-09-27 15:08:59 -0500

    Desarrollos integrales 2
    Listo

--- Assets/Scripts/IntegralBehaviour.cs
+++ Assets/Scripts/IntegralBehaviour.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Crystal;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class IntegralBehaviour : MonoBehaviour
@@ -7,15 +10,50 @@ public class IntegralBehaviour : MonoBehaviour
     [SerializeField, Tooltip("Objetos del menu")] private GameObject[] menu;
 
     [SerializeField, Tooltip("Objetos del contenido")] private GameObject[] content;
-    // Start is called before the first frame update
+
+    //[SerializeField, Tooltip("Script para muesca")] private SafeArea safeArea;
     void Start()
     {
-        
+        Screen.orientation = ScreenOrientation.Portrait; 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ContentShow(int index)
     {
+        MenuHide();
+        foreach (GameObject item in content)
+        {
+            item.SetActive(false);
+        }
+        content[0].SetActive(true);
+        content[index].SetActive(true);
+    }
+    
+    public void ContentHide()
+    {
+        foreach (GameObject item in content)
+        {
+            item.SetActive(false);
+        }
         
+        MenuShow();
+    }
+    
+    
+    private void MenuShow()
+    {
+        //safeArea.enabled = true;
+        foreach (GameObject item in menu)
+        {
+            item.SetActive(true);
+        }
+    }
+    
+    private void MenuHide()
+    {
+        //safeArea.enabled = false;
+        foreach (GameObject item in menu)
+        {
+            item.SetActive(false);
+        }
     }
 }
